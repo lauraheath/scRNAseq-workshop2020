162386e664ebae40d7d8bf91897b6039a37ece8e
Ubuntu <ubuntu@ip-10-41-21-180.ec2.internal>
2020-08-21 18:43:19 +0000

    updating and testing package install

--- scRNAseq_dependencies_packages.R
+++ scRNAseq_dependencies_packages.R
@@ -1,13 +1,15 @@
-#dependencies to run before installing packages:
-#apt-get update -y
-#apt-get install -y curl dpkg-dev zlib1g-dev libssl-dev libffi-dev curl nano less libcurl4-openssl-dev gawk build-essential chrpath libssl-dev libxft-dev libfreetype6 libfreetype6-dev libfontconfig1 libfontconfig1-dev libudunits2-dev libgdal-dev libpython-dev libpython3-dev libgsl-dev
+#dependencies to run in terminal window before installing packages:
+#sudo apt-get update -y
+#sudo apt-get install -y libudunits2-dev libgdal-dev
 
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install(c("Biobase", "BiocGenerics","DelayedArray","DelayedMatrixStats","limma","S4Vectors","SingleCellExperiment","SummarizedExperiment", "batchelor","edgeR","qvalue", "multtest", "scater", "scran"))
-install.packages("Rfast")
-install.packages("synapser", repos=c("http://ran.synapse.org", "http://cran.fhcrc.org"))
-#devtools::install_url('https://cran.r-project.org/src/contrib/Archive/Matrix.utils/Matrix.utils_0.9.7.tar.gz')
+## also set up synapse login configuration in terminal:
+#nano .synapseConfig
+##in nano window type the following:
+#[authentication]
+#username = <username>
+#password = <password>
+
+BiocManager::install(c("Biobase", "BiocGenerics","DelayedArray","DelayedMatrixStats","limma","S4Vectors","SingleCellExperiment","SummarizedExperiment", "batchelor", "Matrix.utils"))
 devtools::install_github('cole-trapnell-lab/leidenbase')
 devtools::install_github('cole-trapnell-lab/monocle3', ref="develop")
 
